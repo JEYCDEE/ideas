9797a22e8a9542003c5c28c50a1c12911070e234
Volodymyr Mon <jeycdee@gmail.com>
2019-07-15 08:18:00 +0000

    Changing markup to match PSR rules

--- php/Pipedrive/example.php
+++ php/Pipedrive/example.php
@@ -10,120 +10,120 @@ $pipedrive = new App\PipedriveManager('YourPipedriveTokenShouldGoHere');
  *
  * * * * DEALS * * * *
  * $pipedrive->getDeals()
- * $pipedrive->getDealsSummary(string $status = '', int $filterId = 0, int $userId = 0, int $stageId = 0)
- * $pipedrive->getDealByName(string $name)
- * $pipedrive->getDealById(int $dealId)
- * $pipedrive->getDealActivities(int $dealId, int $start = 0, int $limit = 0, int $done = -1)
- * $pipedrive->getDealFiles(int $dealId, int $start = 0, int $limit = 0)
- * $pipedrive->getDealUpdates(int $dealId, int $start = 0, int $limit = 0)
- * $pipedrive->getDealFollowers(int $dealId)
- * $pipedrive->getDealMessages(int $dealId, int $start = 0, int $limit = 0)
- * $pipedrive->getDealParticipants(int $dealId, int $start = 0, int $limit = 0)
- * $pipedrive->getDealPermittedUsers(int $dealId, int $accessLevel = 0)
- * $pipedrive->getDealPersons(int $dealId, int $start = 0, int $limit = 0)
- * $pipedrive->getDealProducts(int $dealId, int $start = 0, int $limit = 0)
+ * $pipedrive->getDealsSummary($status = '', $filterId = 0, $userId = 0, $stageId = 0)
+ * $pipedrive->getDealByName($name)
+ * $pipedrive->getDealById($dealId)
+ * $pipedrive->getDealActivities($dealId, $start = 0, $limit = 0, $done = -1)
+ * $pipedrive->getDealFiles($dealId, $start = 0, $limit = 0)
+ * $pipedrive->getDealUpdates($dealId, $start = 0, $limit = 0)
+ * $pipedrive->getDealFollowers($dealId)
+ * $pipedrive->getDealMessages($dealId, $start = 0, $limit = 0)
+ * $pipedrive->getDealParticipants($dealId, $start = 0, $limit = 0)
+ * $pipedrive->getDealPermittedUsers($dealId, $accessLevel = 0)
+ * $pipedrive->getDealPersons($dealId, $start = 0, $limit = 0)
+ * $pipedrive->getDealProducts($dealId, $start = 0, $limit = 0)
  *
  * * * * DEAL FIELDS * * * *
  * $pipedrive->getDealFields()
  *
  * * * * FILES * * * *
- * $pipedrive->getFiles(int $start = 0, int $limit = 0)
- * $pipedrive->getFileById(int $fileId)
+ * $pipedrive->getFiles($start = 0, $limit = 0)
+ * $pipedrive->getFileById($fileId)
  *
  * * * * GLOBAL MESSAGES * * * *
  * $pipedrive->getGlobalMessages()
  *
  * * * * MAIL MESSAGES * * * *
- * $pipedrive->getMailMessageById(int $messageId, int $includeBody = 0)
+ * $pipedrive->getMailMessageById($messageId, $includeBody = 0)
  *
  * * * * MAIL THREADS * * * *
- * $pipedrive->getMailThreads(string $folder = 'inbox', int $start = 0, int $limit = 0)
- * $pipedrive->getMailThreadById(int $threadId)
- * $pipedrive->getMailThreadMessages(int $threadId)
+ * $pipedrive->getMailThreads($folder = 'inbox', $start = 0, $limit = 0)
+ * $pipedrive->getMailThreadById($threadId)
+ * $pipedrive->getMailThreadMessages($threadId)
  *
  * * * * NOTES * * * *
- * $pipedrive->getNotes(int $start = 0, int $limit = 0)
- * $pipedrive->getNoteById(int $noteId)
+ * $pipedrive->getNotes($start = 0, $limit = 0)
+ * $pipedrive->getNoteById($noteId)
  *
  * * * * NOTE FIELDS * * * *
  * $pipedrive->getNoteFields()
  *
  * * * * ORGANIZATIONS * * * *
- * $pipedrive->getOrganizations(int $start = 0, int $limit = 0)
- * $pipedrive->getOrganizationByName(string $name, int $start = 0, int $limit = 0)
- * $pipedrive->getOrganizationDetails(int $id)
- * $pipedrive->getOrganizationActivities(int $id, int $start = 0, int $limit = 0, int $done = -1)
- * $pipedrive->getOrganizationDeals(int $id, int $start = 0, int $limit = 0, string $status = '')
- * $pipedrive->getOrganizationFiles(int $id, int $start = 0, int $limit = 0)
- * $pipedrive->getOrganizationUpdates(int $id, int $start = 0, int $limit = 0)
- * $pipedrive->getOrganizationFollowers(int $id)
- * $pipedrive->getOrganizationMessages(int $id, int $start = 0, int $limit = 0)
- * $pipedrive->getOrganizationPermittedUsers(int $id, int $accessLevel = 0)
- * $pipedrive->getOrganizationPersons(int $id, int $start = 0, int $limit = 0)
+ * $pipedrive->getOrganizations($start = 0, $limit = 0)
+ * $pipedrive->getOrganizationByName($name, $start = 0, $limit = 0)
+ * $pipedrive->getOrganizationDetails($id)
+ * $pipedrive->getOrganizationActivities($id, $start = 0, $limit = 0, $done = -1)
+ * $pipedrive->getOrganizationDeals($id, $start = 0, $limit = 0, $status = '')
+ * $pipedrive->getOrganizationFiles($id, $start = 0, $limit = 0)
+ * $pipedrive->getOrganizationUpdates($id, $start = 0, $limit = 0)
+ * $pipedrive->getOrganizationFollowers($id)
+ * $pipedrive->getOrganizationMessages($id, $start = 0, $limit = 0)
+ * $pipedrive->getOrganizationPermittedUsers($id, $accessLevel = 0)
+ * $pipedrive->getOrganizationPersons($id, $start = 0, $limit = 0)
  *
  * * * * ORGANIZATION FIELDS * * * *
  * $pipedrive->getOrganizationFields()
  *
  * * * * PERSONS * * * *
- * $pipedrive->getPersons(int $start = 0, int $limit = 0)
- * $pipedrive->getPersonByName(string $name, int $start = 0, int $limit = 0)
- * $pipedrive->getPersonDetails(int $personId)
- * $pipedrive->getPersonActivities(int $personId, int $start = 0, int $limit = 0, int $done = -1)
- * $pipedrive->getPersonDeals(int $personId, int $start = 0, int $limit = 0, string $status = '')
- * $pipedrive->getPersonFiles(int $personId, int $start = 0, int $limit = 0)
- * $pipedrive->getPersonUpdates(int $personId, int $start = 0, int $limit = 0)
- * $pipedrive->getPersonFollowers(int $personId)
- * $pipedrive->getPersonMessages(int $personId, int $start = 0, int $limit = 0)
- * $pipedrive->getPersonPermittedUsers(int $personId, int $accessLevel = 0)
- * $pipedrive->getPersonProducts(int $personId, int $start = 0, int $limit = 0)
+ * $pipedrive->getPersons($start = 0, $limit = 0)
+ * $pipedrive->getPersonByName($name, $start = 0, $limit = 0)
+ * $pipedrive->getPersonDetails($personId)
+ * $pipedrive->getPersonActivities($personId, $start = 0, $limit = 0, $done = -1)
+ * $pipedrive->getPersonDeals($personId, $start = 0, $limit = 0, $status = '')
+ * $pipedrive->getPersonFiles($personId, $start = 0, $limit = 0)
+ * $pipedrive->getPersonUpdates($personId, $start = 0, $limit = 0)
+ * $pipedrive->getPersonFollowers($personId)
+ * $pipedrive->getPersonMessages($personId, $start = 0, $limit = 0)
+ * $pipedrive->getPersonPermittedUsers($personId, $accessLevel = 0)
+ * $pipedrive->getPersonProducts($personId, $start = 0, $limit = 0)
  *
  * * * * PERSON FIELDS * * * *
- * $pipedrive->getPersonFields(int $start = 0, int $limit = 0)
- * $pipedrive->getPersonField(int $fieldId)
+ * $pipedrive->getPersonFields($start = 0, $limit = 0)
+ * $pipedrive->getPersonField($fieldId)
  *
  * * * * PIPELINES * * * *
  * $pipedrive->getPipelines()
- * $pipedrive->getPipeline(int $pipelineId)
- * $pipedrive->getPipelineConversionRate(int $pipelineId, DateTime $startDate, DateTime $endDate)
- * $pipedrive->getPipelineDeals(int $pipelineId, int $start = 0, int $limit = 0, int $filterId = 0, int $userId = 0, int $stageId = 0
- * $pipedrive->getPipelineMovements(int $pipelineId, DateTime $startDate, DateTime $endDate)
+ * $pipedrive->getPipeline($pipelineId)
+ * $pipedrive->getPipelineConversionRate($pipelineId, $startDate, $endDate)
+ * $pipedrive->getPipelineDeals($pipelineId, $start = 0, $limit = 0, $filterId = 0, $userId = 0, $stageId = 0
+ * $pipedrive->getPipelineMovements($pipelineId, $startDate, $endDate)
  *
  * * * * PRODUCTS * * * *
- * $pipedrive->getProducts(int $start = 0, int $limit = 0, int $userId = 0, int $filterId = 0)
- * $pipedrive->getProductsByName(string $name, int $start = 0, int $limit = 0)
- * $pipedrive->getProductById(int $productId)
- * $pipedrive->getProductDeals(int $productId, int $start = 0, int $limit = 0, string $status = '')
- * $pipedrive->getProductFiles(int $productId, int $start = 0, int $limit = 0)
- * $pipedrive->getProductFollowers(int $productId)
- * $pipedrive->getProductPermittedUsers(int $productId, int $accessLevel = 0)
+ * $pipedrive->getProducts($start = 0, $limit = 0, $userId = 0, $filterId = 0)
+ * $pipedrive->getProductsByName($name, $start = 0, $limit = 0)
+ * $pipedrive->getProductById($productId)
+ * $pipedrive->getProductDeals($productId, $start = 0, $limit = 0, $status = '')
+ * $pipedrive->getProductFiles($productId, $start = 0, $limit = 0)
+ * $pipedrive->getProductFollowers($productId)
+ * $pipedrive->getProductPermittedUsers($productId, $accessLevel = 0)
  *
  * * * * PRODUCT FIELDS * * * *
  * $pipedrive->getProductFields()
- * $pipedrive->getProductField(int $fieldId)
+ * $pipedrive->getProductField($fieldId)
  *
  * * * * ROLES * * * *
- * $pipedrive->getRoles(int $start = 0, int $limit = 0)
- * $pipedrive->getRoleById(int $roleId)
- * $pipedrive->getRoleAssignments(int $roleId, int $start = 0, int $limit = 0)
- * $pipedrive->getRoleSubRoles(int $roleId, int $start = 0, int $limit = 0)
- * $pipedrive->getRoleSettings(int $roleId)
+ * $pipedrive->getRoles($start = 0, $limit = 0)
+ * $pipedrive->getRoleById($roleId)
+ * $pipedrive->getRoleAssignments($roleId, $start = 0, $limit = 0)
+ * $pipedrive->getRoleSubRoles($roleId, $start = 0, $limit = 0)
+ * $pipedrive->getRoleSettings($roleId)
  *
  * * * * STAGES * * * *
- * $pipedrive->getStages(int $pipelineId = 0)
- * $pipedrive->getStageById(int $stageId)
- * $pipedrive->getStageDeals(int $stageId, int $start = 0, int $limit = 0, int $filterId = 0, int $userId = 0)
+ * $pipedrive->getStages($pipelineId = 0)
+ * $pipedrive->getStageById($stageId)
+ * $pipedrive->getStageDeals($stageId, $start = 0, $limit = 0, $filterId = 0, $userId = 0)
  *
  * * * * USERS * * * *
  * $pipedrive->getUsers()
- * $pipedrive->getUsersByName(string $name)
- * $pipedrive->getUsersByEmail(string $email)
+ * $pipedrive->getUsersByName($name)
+ * $pipedrive->getUsersByEmail($email)
  * $pipedrive->getUserData()
- * $pipedrive->getUserById(int $userId)
- * $pipedrive->getUserBlacklistedEmails(int $userId)
- * $pipedrive->getUserFollowers(int $userId)
- * $pipedrive->getUserPermissions(int $userId)
- * $pipedrive->getUserRoleAssignments(int $userId, int $start = 0, int $limit = 0)
- * $pipedrive->getUserRoleSettings(int $userId)
+ * $pipedrive->getUserById($userId)
+ * $pipedrive->getUserBlacklistedEmails($userId)
+ * $pipedrive->getUserFollowers($userId)
+ * $pipedrive->getUserPermissions($userId)
+ * $pipedrive->getUserRoleAssignments($userId, $start = 0, $limit = 0)
+ * $pipedrive->getUserRoleSettings($userId)
  * $pipedrive->getUserConnections()
  * $pipedrive->getUserSettings()
  *
@@ -132,11 +132,4 @@ $pipedrive = new App\PipedriveManager('YourPipedriveTokenShouldGoHere');
  *
  */
 
-try {
-
-    var_dump($pipedrive->getDeals());
-
-} catch (\Exception $e) {
-
-
-}
+var_dump($pipedrive->getDeals());
